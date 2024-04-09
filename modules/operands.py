462363d216822fc0c837d9d55e1cebff6e5c391f
PSU-Maica <202280193@psu.palawan.edu.ph>
2024-04-10 03:09:49 +0800

    function for get_operands

---
+++ modules/operands.py
@@ -0,0 +1,20 @@
+operators = ['-', '.', '+']
+delimiter = ['(', ')', ' ']
+symbols = operators + delimiter
+
+def get_operands(expression):
+    operands = []
+    dist_opr = []
+
+    for e in expression:
+        if e not in symbols:
+            operands.append(e)
+
+    dist_opr.append(operands[0])      
+    for operand in operands:
+        temp_var = operands[0]    
+        if temp_var != operand:
+            dist_opr.append(operand)
+            temp_var = operand
+            
+    return dist_opr
---
+++ scratch.py
@@ -0,0 +1,26 @@
+# detecting variables
+
+operators = ['-', '.', '+']
+delimiter = ['(', ')', ' ']
+symbols = operators + delimiter
+
+expression = "AB + AC"
+        
+def get_operands(expression):
+    operands = []
+    dist_opr = []
+
+    for e in expression:
+        if e not in symbols:
+            operands.append(e)
+
+    dist_opr.append(operands[0])      
+    for operand in operands:
+        temp_var = operands[0]    
+        if temp_var != operand:
+            dist_opr.append(operand)
+            temp_var = operand
+            
+    return dist_opr
+
+print(get_operands(expression))
